ebf6e518055a55c15ca4482ed617b8f3ba19bf19
sgreilich <sgreilich@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-08-02 07:57:01 +0000

    Bug fixed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hitxml@57 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exec/HIT_XML.Plan_to_SOBP.R
+++ pkg/exec/HIT_XML.Plan_to_SOBP.R
@@ -1,6 +1,8 @@
 rm(list = ls())
 
 library(XML)
+library(ggplot2)
+library(data.table)
 
 doc <- XML::xmlTreeParse("bio_c_1Gy_8mm_35mm_1x25_schmal.xml", useInternalNodes = TRUE)
 
@@ -10,17 +12,17 @@ ll   <- lapply(IESs,
          # x <- IESs[[1]]
          energy.GeV <- as.numeric(XML::xmlGetAttr(x, name = "energy"))/1000.0
          focus.cm   <- as.numeric(XML::xmlGetAttr(x, name = "focus"))/10.0
-         pos.x.cm   <- sapply(XML::getNodeSet(x, path = "//Voxel"),
+         pos.x.cm   <- sapply(XML::xmlElementsByTagName(x, name = "Voxel"),
                               function(y){
                                 as.numeric(XML::xmlGetAttr(y, name = "x")) / 10.0
                               })
-         pos.y.cm   <- sapply(XML::getNodeSet(x, path = "//Voxel"),
+         pos.y.cm   <- sapply(XML::xmlElementsByTagName(x, name = "Voxel"),
                               function(y){
                                 as.numeric(XML::xmlGetAttr(y, name = "y")) / 10.0
                               })
-         particles  <- sapply(XML::getNodeSet(x, path = "//Voxel"),
+         particles  <- sapply(XML::xmlElementsByTagName(x, name = "Voxel"),
                               function(y){
-                                as.numeric(XML::xmlGetAttr(y, name = "particles")) / 10.0
+                                as.numeric(XML::xmlGetAttr(y, name = "particles"))
                               })
          return(data.frame(energy.GeV = energy.GeV,
                            pos.x.cm = pos.x.cm,
@@ -29,10 +31,33 @@ ll   <- lapply(IESs,
                            particles = particles))
        })
 
-df <- do.call("rbind", ll)
+df <- as.data.table(do.call("rbind", ll))
+
+tapply(df$particles, df$energy.GeV, unique)
+
+df2 <- df[,
+       .(energy.GeV = unique(energy.GeV),
+         pos.x.cm  = 0.0,
+         pos.y.cm  = 0.0,
+         focus.cm  = unique(focus.cm),
+         total.particles = sum(particles)),
+       by = .(energy.GeV)]
+
+ggplot(df, aes(x = pos.x.cm, y = pos.y.cm)) +
+  theme_bw() +
+  geom_point() +
+  facet_wrap("energy.GeV")
+
 
 write.table(df,
             file = "SOBP_from_Plan.dat",
+            quote = FALSE,
+            row.names = FALSE,
+            col.names = FALSE,
+            eol       = "\r\n" )
+
+write.table(df2,
+            file = "SOBP_from_Plan_simple.dat",
             quote = FALSE,
             row.names = FALSE,
             col.names = FALSE,
